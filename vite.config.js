8832fd305445bf31097cbb789d7f65b3f3e7d849
pekaboo01 <167494554+pekaboo01@users.noreply.github.com>
2025-08-30 14:40:17 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser'
+    outDir: 'dist'
   }
 })
